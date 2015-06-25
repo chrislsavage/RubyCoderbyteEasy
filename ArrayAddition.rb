22bc4d50f8daefb98ff18bebeb4fb3d26709c9a1
chrislsavage <billing@hcproject.org>
2015-06-25 11:52:39 -0400

    Complete

--- ArrayAddition.rb
+++ ArrayAddition.rb
@@ -4,7 +4,7 @@
 #  in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3]
 #  the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain
 #  all the same elements, and may contain negative numbers.
-#Use the Parameter Testing feature in the box below to test your code with different arguments. 
+#Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 
 def ArrayAdditionI(arr)
@@ -12,21 +12,26 @@ def ArrayAdditionI(arr)
   max = arr[-1]
   arr.delete(max)
   newarr = arr
+  sum = 0
     i = 0
-    othernum = 0
     j = 0
-    until j == nil do
-        i = 0
         while i < arr.length
+        sum += arr[i]
             while j < arr.length
-            othernum = arr[j]
-                if othernum + arr[i] == max
-                return true
-                end
+                if  arr[j] != arr[i]
+                    puts sum.to_s + ":"
+                    sum += arr[j]
+                    puts sum
+                    if sum == max
+                    return true
+                    end
+                 end
             j += 1
             end
-         i += 1
-        end
-    end
+        sum = 0
+        j= 0
+        i += 1
+    	end
   return false
 end
+   
