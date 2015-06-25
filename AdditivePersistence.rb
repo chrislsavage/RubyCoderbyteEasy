093e3946cf5a2c35dbd82ab43c67b3f3719eba00
chrislsavage <billing@hcproject.org>
2015-06-25 11:30:07 -0400

    Complete

--- AdditivePersistence.rb
+++ AdditivePersistence.rb
@@ -7,33 +7,14 @@
 # 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9.
 
 
-def AdditivePersistence(num)
-num.split("")
-count = 0
-  i = 0
-  sum =0
-  while i < 0
-    sum += num[i].to_i
-    i+=1
-  end
-  if sum.length > 1
-    AdditivePersistence(sum)
-  end
-else
 
-  # code goes here
-  return sum
 
-end
-
-# keep this function call here
-# to see how to enter arguments in Ruby scroll down
-AdditivePersistence(STDIN.gets)
 
 
 def NumSum (num)
     num = num.to_s.split("")
     i = 0
+    sum = 0
   while i < num.length
     sum += num[i].to_i
     i+=1
@@ -44,49 +25,11 @@ end
 
 def AdditivePersistence(num)
 count = 0
-
 newnum = num
-
-  if num < 10
-    return count
-  end
+while newnum > 10
+newnum = NumSum(newnum)
   count += 1
-  NumSum(newnum)
-
+end
+return count
 
 end
-
-# keep this function call here
-# to see how to enter arguments in Ruby scroll down
-AdditivePersistence(19)
-
-
-
-
-def Adding(num)
-        num = num.to_s.split("")
-count = 0
-  i = 0
-  sum =0
-  while i < 0
-    sum += num[i].to_i
-    i+=1
-  end
-  return sum
-  end
-
-
-def AdditivePersistence(num)
-count = 0
-sum = num
-if sum > 9
-    Adding(sum)
-else
-    return count
-  end
-
-end
-
-# keep this function call here
-# to see how to enter arguments in Ruby scroll down
-AdditivePersistence(19)
--- DashInsert.rb
+++ DashInsert.rb
@@ -7,19 +7,16 @@ def DashInsert(str)
   str = str.split("")
   odd = "13579"
   i = 0
-  puts str.join("")
+  new = ""
 
  while i < str.length
-    if odd.index(str[i]) >= 0 && odd.index(str[i + 1]) >= 0
-      str.insert(i+1, "-")
-      i+=1
+   new += str[i]
+    if odd.index(str[i])  && str[i+1] && odd.index(str[i + 1])
+      new += "-"
     end
-
+i+= 1
   end
   # code goes here
-  return str.join("")
+ return new
 
 end
-
-# keep this function call here
-DashInsert("5667")
