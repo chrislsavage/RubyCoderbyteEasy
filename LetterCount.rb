03f908a2b2e38da72cd232f4b9339a8dd782f00b
chrislsavage <billing@hcproject.org>
2015-06-25 08:40:02 -0400

    Added Question

--- LetterCount.rb
+++ LetterCount.rb
@@ -3,7 +3,7 @@
 #number of repeated letters. For example: "Today, is the greatest day ever!"
 #should return greatest because it has 2 e's (and 2 t's) and it comes
 #before ever which also has 2 e's. If there are no words with repeating
-#letters return -1. Words will be separated by spaces.  
+#letters return -1. Words will be separated by spaces.
 
 def LetterCountI(str)
 
